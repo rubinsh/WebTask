61084d992408151fa9929ac2e00ce669191041d9
Shai Rubinshtein <rubinsh@gmail.com>
2011-02-20 21:16:37 +0200

    going to open tasks after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to tasks_path, :notice => "Logged in successfully"
+      redirect_to tasks_path(:type => 'not_completed'), :notice => "Logged in successfully"
     else
       if params[:email] != nil
         flash.now[:alert] = "Invalid login/password combination"
