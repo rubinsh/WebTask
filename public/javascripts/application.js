2724c36ef046e13488e62cbd4719c9a589907a01
Shai Rubinshtein <rubinsh@gmail.com>
2011-03-10 09:36:15 +0200

    set the styling - again...

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+    $("#task_due_date").datepicker({ dateFormat: 'yy-mm-dd' });
+    //"option", "altFormat", 'yy-mm-dd'
+
+
+});
+
+$(function() {
+    $( "#accordion" ).accordion();
+});
+
+$(function() {
+    // Assign a click handler that grabs the URL
+    // from the first cell and redirects the user.
+    $('#task-table tr').click(function ()
+    {
+        location.href = $(this).find('#task_link').attr('href');
+    });
+});
