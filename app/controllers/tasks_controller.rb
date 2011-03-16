de7765013615fbc11d823ff52295381f268e3966
Shai Rubinshtein <rubinsh@gmail.com>
2011-03-16 20:23:03 +0200

    add tiny_mce text editor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,16 @@
 class TasksController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :new, :create]
+  uses_tiny_mce :options => {
+      :theme => 'advanced',
+      :theme_advanced_toolbar_location => 'top',
+      :theme_advanced_toolbar_align => 'left',
+      :mode => 'textareas',
+      :plugins => %w{ directionality },
+      :theme_advanced_buttons1 => "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,fontselect,fontsizeselect",
+      :theme_advanced_buttons2 => "bullist,numlist,sparator,undo,redo,separator,ltr,rtl,separator,forecolor,backcolor,separator,link,unlink",
+      :theme_advanced_buttons3 => ""
+  }
 
   # GET /tasks
   # GET /tasks.xml
